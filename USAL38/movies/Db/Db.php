c675bb7f57788ea81e9b1bcd57ea863c9a467b0b
BMuller-dev <71022676+BMuller-dev@users.noreply.github.com>
2021-12-15 16:25:17 +0100

    1

--- USAL38/movies/Db/Db.php
+++ USAL38/movies/Db/Db.php
@@ -11,14 +11,14 @@ $toto = new DbConnect();
 
 // $host = "localhost"; // Adresse du serveur de base de données
 // $port = "3306"; // Port du serveur de base de données
-$user = "root"; // Nom d'utilisateur pour la connexion à la base de données
-$pass = ""; // Mot de passe pour la connexion à la base de données
+$username = "root"; // Nom d'utilisateur pour la connexion à la base de données
+$password = ""; // Mot de passworde pour la connexion à la base de données
 
 // Chaine de connexion = Data Source Name
 $dsn = "mysql:host=localhost;port=3306;dbname=db_movies;charset=utf8";
 
 // Établissement de la connexion
-$connection = new PDO($dsn, $user, $pass);
+$connection = new PDO($dsn, $username, $password);
 // var_dump($connection);
 
 // Création d'un requête SQl
--- USAL38/movies/Db/DbConnect.php
+++ USAL38/movies/Db/DbConnect.php
@@ -19,9 +19,22 @@ class DbConnect
 
     /**
      *  CONSTRUCTOR 
+     * @param string $host Database Hostname
+     * @param int $port Database port
+     * @param string $dbname Database name
+     * @param string $username Database username
+     * @param string $password Database password
      */
     public function __construct(string $host, int $port, string $dbname, string $username, string $password)
     {
         $this->host = $host;
+        $this->port = $port;
+        $this->dbname = $dbname;
+        $this->username = $username;
+        $this->password = $password;
+
+        $dsn = "mysql:$this->host=$host;$this->port=$port;$this->dbname=$dbname;charset=utf8";
+
+        $connection = new PDO($dsn, $username, $password);
     }
 } // End of class
