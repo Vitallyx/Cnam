d09626613331d40892d02b40b6d5a896c7a4323e
BMuller-dev <71022676+BMuller-dev@users.noreply.github.com>
2022-01-14 16:49:37 +0100

    1

--- USAL38/exo_sql/exo1.sql
+++ USAL38/exo_sql/exo1.sql
@@ -7,3 +7,9 @@ SELECT * FROM villes_france_free ORDER BY ville_population_2012 DESC LIMIT 0,10;
 SELECT * FROM villes_france_free ORDER BY ville_surface ASC LIMIT 50;
 
 /* Obtenir la liste des départements d’outres-mer, c’est-à-dire ceux dont le numéro de département commencent par “97” */
+SELECT * FROM departement WHERE departement_code LIKE '97%';
+
+/* Obtenir le nom des 10 villes les plus peuplées en 2012, ainsi que le nom du département associé */
+SELECT ville_nom, departement_nom, ville_population_2012 FROM villes_france_free as V
+JOIN departement as D ON V.ville_departement = D.departement_code
+ORDER BY ville_population_2012 DESC LIMIT 0,10;
